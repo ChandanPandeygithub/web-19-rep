414fef753527387fb823f0610b538c9dda0f02e4
chandan pandey <77069375+ChandanPandeygithub@users.noreply.github.com>
2022-12-02 19:55:59 +0530

    RCT101_B21 Day 7 :UseEffect-2 CP PLATFORM ASSIGNMENT
    CP PLATFORM ASSIGNMENT

---
+++ React Mobile OS/src/components/MobileOS.jsx
@@ -0,0 +1,26 @@
+
+
+import React from "react";
+
+function MobileOS(){
+
+    return(<div>
+        <h1>Mobile Operating System</h1>
+        <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iphone</li>
+        <li>Windows phone</li>
+        </ul>
+          
+        <h1>Mobile Manufactures</h1>
+        <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+        </ul>
+        </div>)
+}
+
+export default MobileOS;
