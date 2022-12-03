d6ffbc29883ad7eb99a6a34e40cbe3bed3dfd02e
chandan pandey <77069375+ChandanPandeygithub@users.noreply.github.com>
2022-12-03 16:31:42 +0530

    RCT101_B21 Day 8 : useRef hook and FormsCOUNT DOWN TIMER
    COUNT DOWN TIMER

---
+++ count-down-timer/src/Component/Timer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Timer(){
+  const[time,setTime]= React.useState(1);
+  const ref = React.useRef(null);
+
+  function handleStart(){
+    if(ref.current!=null){
+        return ;
+    }
+    ref.current= setInterval(()=>{
+        setTime((pretime)=>pretime+1);
+     },1000);
+  }
+  function handlePush(){
+    clearInterval(ref.current);
+  }
+  function handleStop(){
+    handlePush();
+   ref.current=null;
+  }
+   function handleReset(){
+     handleStop()
+     setTime(0);
+  }
+    return(<div>
+        <h1>{time}</h1>
+        <button onClick={handleStart}>start</button>
+        
+        <button onClick={handleStop}>stop</button>
+        <button onClick={handleReset}>reset</button>
+        </div>)
+}
+export default Timer;
